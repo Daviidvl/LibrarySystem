ea88a63935b4fb200b21527bbb38a83d36776b35
Davi Pereira <145718575+Daviidvl@users.noreply.github.com>
2026-08-31 22:18:27 -0300

    feat: criei a funcao de cadastrar produto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,28 +8,62 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        List<Books> livros = new ArrayList<>();
-        livros.add(new Books("Harry Potter e a Pedra Filosofal", "J.K. Rowling", "Fantasia"));
-        livros.add(new Books("O Senhor dos Anéis", "J.R.R. Tolkien", "Fantasia"));
-        livros.add(new Books("Dom Casmurro", "Machado de Assis", "Romance"));
+        List<Books> book = new ArrayList<>();
 
-        System.out.println("Digite o nome do livro, que deseja alugar: ");
-        String nome = sc.nextLine();
+        int opcao = 5;
 
-        boolean encontrado = false;
+        while (opcao != 3) {
+            System.out.println("Digite a opção que deseja realizar a baixo");
+            System.out.println("1. Cadastrar um livro ");
+            System.out.println("2. Alugar um livro");
+            System.out.println("3. Sair");
+            opcao = sc.nextInt();
+            sc.nextLine();
 
-        for (Books livro : livros){
-            if (livro.getNome().equalsIgnoreCase(nome)){
-                System.out.println("Livro encontrado! ");
-                System.out.println("Autor: " + livro.getAutor());
-                System.out.println("Genero: " + livro.getGenero());
-                encontrado = true;
-                break;
+            switch (opcao) {
+                case 1:
+                    System.out.println();
+                    System.out.println("Digite nome do livro que deseja cadastrar: ");
+                    String nome = sc.nextLine();
+                    System.out.println("Digite o nome do autor: ");
+                    String autor = sc.nextLine();
+                    System.out.println("Digite o genero do livro: ");
+                    String genero = sc.nextLine();
+
+                    Books livros = new Books(nome, autor, genero);
+                    book.add(livros);
+                    System.out.println("Livro cadastrado com sucesso!");
+                    break;
+
+                case 2:
+                    System.out.println("Digite o nome do livro, que deseja alugar: ");
+                    nome = sc.nextLine();
+
+                    boolean encontrado = false;
+
+                    for (Books books : book) {
+                        if (books.getNome().equalsIgnoreCase(nome)) {
+                            System.out.println("Livro encontrado! ");
+                            System.out.println("Autor: " + books.getAutor());
+                            System.out.println("Genero: " + books.getGenero());
+                            encontrado = true;
+                            break;
+                        }
+                    }
+
+                    if (!encontrado) {
+                        System.out.println("Livro não encontrado");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida, tente novamente.");
+                    break;
             }
-        }
-
-        if (!encontrado){
-            System.out.println("Livro não encontrado");
         }
         sc.close();
     }
